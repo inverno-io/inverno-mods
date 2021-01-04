e267bfb47ff03fb44b65b1884061624fa3ea810b
Jeremy Kuhn <jeremy.kuhn@winterframework.io>
2021-01-04 17:29:08 +0100

    Add missing licence headers

--- winter-web/src/main/java/io/winterframework/mod/web/internal/server/http2/Http2ChannelHandler.java
+++ winter-web/src/main/java/io/winterframework/mod/web/internal/server/http2/Http2ChannelHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Jeremy KUHN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.winterframework.mod.web.internal.server.http2;
 
 import io.netty.buffer.ByteBuf;
@@ -26,6 +41,11 @@ import io.winterframework.mod.web.ResponseBody;
 import io.winterframework.mod.web.internal.RequestBodyDecoder;
 import io.winterframework.mod.web.internal.server.AbstractExchange;
 
+/**
+ * 
+ * @author jkuhn
+ *
+ */
 public class Http2ChannelHandler extends Http2ConnectionHandler implements Http2FrameListener, Http2Connection.Listener {
 
 	private ExchangeHandler<RequestBody, ResponseBody, Exchange<RequestBody, ResponseBody>> rootHandler; 
