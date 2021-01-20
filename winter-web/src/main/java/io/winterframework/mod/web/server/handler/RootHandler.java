18273af561cec40ed2d70a09fcf481598db232e4
Jeremy Kuhn <jeremy.kuhn@inverno.io>
2021-01-20 16:40:06 +0100

    Remove favicon.ico handler from web module

--- winter-web/src/main/java/io/winterframework/mod/web/server/handler/RootHandler.java
+++ winter-web/src/main/java/io/winterframework/mod/web/server/handler/RootHandler.java
@@ -15,14 +15,11 @@
  */
 package io.winterframework.mod.web.server.handler;
 
-import java.net.URI;
 import java.util.function.Supplier;
 
 import io.winterframework.core.annotation.Bean;
 import io.winterframework.core.annotation.Overridable;
 import io.winterframework.core.annotation.Wrapper;
-import io.winterframework.mod.base.resource.Resource;
-import io.winterframework.mod.base.resource.ResourceService;
 import io.winterframework.mod.web.NotFoundException;
 import io.winterframework.mod.web.server.Exchange;
 import io.winterframework.mod.web.server.ExchangeHandler;
@@ -36,22 +33,11 @@ import io.winterframework.mod.web.server.ExchangeHandler;
 @Overridable 
 public class RootHandler implements Supplier<ExchangeHandler<Exchange>> {
 
-	private ResourceService resourceService;
-	
-	public RootHandler(ResourceService resourceService) {
-		this.resourceService = resourceService;
-	}
-	
 	@Override
 	public ExchangeHandler<Exchange> get() {
 		return exchange -> {
-			if(exchange.request().headers().getPath().equalsIgnoreCase("/favicon.ico")) {
-				try(Resource favicon = this.resourceService.get(URI.create("classpath:/winter_favicon.svg"))) {
-					exchange.response().body().resource().data(favicon);
-				} 
-				catch (Exception e) {
-					throw new NotFoundException();
-				}
+			if(exchange.request().headers().getPath().equalsIgnoreCase("/")) {
+				exchange.response().body().raw().data("Hello!");
 			}
 			else {
 				throw new NotFoundException();
