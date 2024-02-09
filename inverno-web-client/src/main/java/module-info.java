85783a4b22ff6235dfade0f723d4aa83705594c7
Jeremy Kuhn <jeremy.kuhn@inverno.io>
2024-02-09 23:08:40 +0100

    Init Web client module

---
+++ inverno-web-client/src/main/java/module-info.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2024 Jeremy KUHN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * <p>
+ * The Inverno framework Web client module provides a Web enabled HTTP1.x and HTTP/2 client.
+ * </p>
+ * 
+ * @author <a href="mailto:jeremy.kuhn@inverno.io">Jeremy Kuhn</a>
+ * @since 1.7
+ */
+@io.inverno.core.annotation.Module
+module io.inverno.mod.web.server {
+	requires io.inverno.core;
+	requires static io.inverno.core.annotation; // for javadoc...
+	
+	requires transitive io.inverno.mod.base;
+	requires io.inverno.mod.configuration;
+	requires transitive io.inverno.mod.http.client;
+
+	requires org.apache.commons.text;
+	requires org.apache.logging.log4j;
+	requires reactor.core;
+	requires org.reactivestreams;
+	
+	exports io.inverno.mod.web.client;
+}
