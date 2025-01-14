6906156e59d4b7da97197ecdc561b85609e7bb6e
Jeremy Kuhn <jeremy.kuhn@inverno.io>
2025-01-14 09:38:44 +0100

    Fix tests

--- inverno-mods-test/src/test/java/io/inverno/mod/test/http/HttpClientSpecificTest.java
+++ inverno-mods-test/src/test/java/io/inverno/mod/test/http/HttpClientSpecificTest.java
@@ -84,7 +84,9 @@ public class HttpClientSpecificTest {
 			.moduleOverride(AbstractInvernoModTest.MODULE_OVERRIDE)
 			.annotationProcessorModuleOverride(AbstractInvernoModTest.ANNOTATION_PROCESSOR_MODULE_OVERRIDE)
 			.build();
-		
+
+		// TODO this is causing stackoverflow error in compiler, an alternate approach might be to precompile the module before test and start it with different configurations in order to avoid
+		//  multiple compilations. The drawback is that the test is no longer isolated and becomes dependent on that preprocessing phase.
 //		invernoCompiler.cleanModuleTarget();
 
 		InvernoModuleLoader moduleLoader = invernoCompiler.compile(MODULE_WEBROUTE);
